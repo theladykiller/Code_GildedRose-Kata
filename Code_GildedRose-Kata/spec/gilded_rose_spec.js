844a10c8075669862d4605225540e8473a42f0cb
theladykiller <niki.plachetzky@gmail.com>
2024-06-03 16:16:20 +0200

    added successful test case 1

--- Code_GildedRose-Kata/spec/gilded_rose_spec.js
+++ Code_GildedRose-Kata/spec/gilded_rose_spec.js
@@ -1,9 +1,12 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+  //1.
+  it("name = foo, sell_in = 5, quality = 4", function() {
+    items = [ new Item("foo", 5, 4) ];
+    expect(items[0].name).toEqual("foo");
+    expect(items[0].sell_in).toEqual(5);
+    expect(items[0].quality).toEqual(4);
   });
+
 
 });
